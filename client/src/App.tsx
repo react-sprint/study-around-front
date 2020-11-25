e9cb3de5e0b6b35f92002fef7b973916d4c4f892
OhSeungYoung <osy0056@khu.ac.kr>
2020-11-25 15:06:24 +0900

    chore: Set up the Redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Main from './pages/Main';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Main} />
+        <Route exact path="/" component={MainPage} />
       </Switch>
     </BrowserRouter>
   );
