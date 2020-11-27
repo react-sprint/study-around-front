d830d0853f3e857b5fec52011a9b9d1e88836d93
Jaeuk-YOO <ryou9305@naver.com>
2020-11-28 07:42:40 +0900

    Docs: write about an error handling solution for ts setting

---
+++ client/src/pages/Signup/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import SignUpConatiner from 'composition/SignUp';
+import SignUpPresenter from './SignUpPresenter';
+
+function SignUp() {
+  return (
+    <>
+      {/* <SignUpConatiner /> */}
+      <SignUpPresenter />
+    </>
+  );
+}
+
+export default SignUp;
