a87b2eeb0ea37de742d6565a9fb25626acc7be63
OhSeungYoung <osy0056@khu.ac.kr>
2020-12-04 03:28:48 +0900

    refactor: Add function for loop

--- client/src/composition/StudyPages/StudyPageForm/TimeSelect.tsx
+++ client/src/composition/StudyPages/StudyPageForm/TimeSelect.tsx
@@ -1,7 +1,22 @@
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
 
-function TimeSelect() {
+function TimeSelect(): JSX.Element {
+  const get_loop_options = (
+    startIndex: number,
+    endIndex: number,
+    offset: number
+  ): JSX.Element[] => {
+    const options: JSX.Element[] = [];
+    for (startIndex; startIndex < endIndex; startIndex += offset) {
+      options.push(
+        <option value={startIndex} key={startIndex}>
+          {startIndex}
+        </option>
+      );
+    }
+    return options;
+  };
   return (
     <Fragment>
       <Layout>
@@ -10,77 +25,10 @@ function TimeSelect() {
         <option value="PM">PM</option>
         </SelectAPM>
         */}
-        <SelectTime>
-          <option value="0">00</option>
-          <option value="1">01</option>
-          <option value="2">02</option>
-          <option value="3">03</option>
-          <option value="4">04</option>
-          <option value="5">05</option>
-          <option value="6">06</option>
-          <option value="7">07</option>
-          <option value="8">08</option>
-          <option value="9">09</option>
-          <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-          <option value="13">13</option>
-          <option value="14">14</option>
-          <option value="15">15</option>
-          <option value="16">16</option>
-          <option value="17">17</option>
-          <option value="18">18</option>
-          <option value="19">19</option>
-          <option value="20">20</option>
-          <option value="21">21</option>
-          <option value="22">22</option>
-          <option value="23">23</option>
-        </SelectTime>
-        :
-        <SelectTime>
-          <option value="0">00</option>
-          <option value="10">10</option>
-          <option value="20">20</option>
-          <option value="30">30</option>
-          <option value="40">40</option>
-          <option value="50">50</option>
-        </SelectTime>
-        ~
-        <SelectTime>
-          <option value="1">01</option>
-          <option value="2">02</option>
-          <option value="3">03</option>
-          <option value="4">04</option>
-          <option value="5">05</option>
-          <option value="6">06</option>
-          <option value="7">07</option>
-          <option value="8">08</option>
-          <option value="9">09</option>
-          <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-          <option value="13">13</option>
-          <option value="14">14</option>
-          <option value="15">15</option>
-          <option value="16">16</option>
-          <option value="17">17</option>
-          <option value="18">18</option>
-          <option value="19">19</option>
-          <option value="20">20</option>
-          <option value="21">21</option>
-          <option value="22">22</option>
-          <option value="23">23</option>
-          <option value="24">24</option>
-        </SelectTime>
-        :
-        <SelectTime>
-          <option value="0">00</option>
-          <option value="10">10</option>
-          <option value="20">20</option>
-          <option value="30">30</option>
-          <option value="40">40</option>
-          <option value="50">50</option>
-        </SelectTime>
+        <SelectTime>{get_loop_options(0, 24, 1)}</SelectTime>:
+        <SelectTime>{get_loop_options(0, 51, 10)}</SelectTime>~
+        <SelectTime>{get_loop_options(0, 24, 1)}</SelectTime>:
+        <SelectTime>{get_loop_options(0, 51, 10)}</SelectTime>
       </Layout>
     </Fragment>
   );
